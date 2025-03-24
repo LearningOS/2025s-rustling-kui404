2bf16356a5f896dead32b3c3914960726a3a0944
王娜 <huakui@gmail.com>
2025-03-24 21:24:11 +0800

    update

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,7 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
@@ -41,6 +40,15 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red,green,blue) = tuple;
+        if !(0..=255).contains(&red) ||!(0..=255).contains(&green) ||!(0..=255).contains(&blue){
+            return Err(IntoColorError::IntConversion);
+        }
+        Ok(Color { 
+            red: red as u8, 
+            green: green as u8, 
+            blue: blue as u8,
+             })
     }
 }
 
@@ -48,15 +56,40 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
-    }
+        let [red,green,blue]= arr;
+        if !(0..=255).contains(&red) ||!(0..=255).contains(&green) ||!(0..=255).contains(&blue){
+            return Err(IntoColorError::IntConversion);
+        }
+        Ok(Color { 
+            red: red as u8, 
+            green: green as u8, 
+            blue: blue as u8,
+             })
+      
+}
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3{
+            return Err(IntoColorError::BadLen);
+        }
+        let red = slice[0];
+        let green = slice[1];
+        let blue = slice[2];
+        if !(0..=255).contains(&red) ||!(0..=255).contains(&green) ||!(0..=255).contains(&blue){
+            Err(IntoColorError::IntConversion)
+        }else {
+            Ok(Color { 
+                red: red as u8, 
+                green: green as u8, 
+                blue: blue as u8 })
+        }
+
     }
-}
+    }
 
 fn main() {
     // Use the `try_from` function
