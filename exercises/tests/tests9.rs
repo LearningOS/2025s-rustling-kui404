17e575168813eaaeae1749c214cfc4408284e94b
王娜 <huakui@gmail.com>
2025-03-25 08:48:27 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -32,7 +32,7 @@ extern "Rust" {
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
- mod Foo {
+ mod Foo{
     // No `extern` equals `extern "Rust"`.
     #[no_mangle]
     pub fn my_demo_function(a: u32) -> u32 {
